6b644c6273cea937c1b2912241ea5d844db6c4e0
AbdoNayer <abdelwahabnayer20@gmail.com>
2023-10-12 01:09:54 +0300

    subscription-page

--- js/script.js
+++ js/script.js
@@ -59,6 +59,13 @@ $(document).ready(function () {
         }
     });
 
+    // Upolad Files
+    $('#file-upload').change(function() {
+        var i = $(this).prev('label').clone();
+        var file = $('#file-upload')[0].files[0].name;
+        $(this).prev('label').text(file);
+    });
+
     // Animation Page
     AOS.init();
     
