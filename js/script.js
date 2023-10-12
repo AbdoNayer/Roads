09cc7b53da9add5d0cb198b32ad71e7a6ece0349
AbdoNayer <abdelwahabnayer20@gmail.com>
2023-10-13 00:30:20 +0300

    done-app

--- js/script.js
+++ js/script.js
@@ -19,6 +19,16 @@ $(window).on('load', function () {
 
 $(document).ready(function () {
 
+    // // ADD IMAGE
+    $('.image-uploader').change(function (event){
+        $(this).parents('.images-upload-block').append('<div class="uploaded-block"><img src="'+ URL.createObjectURL(event.target.files[0]) +'"><button class="close"><i class="icon-trash text-white"></i></button></div>');
+    });
+    
+    // REMOVE IMAGE
+    $('.images-upload-block').on('click', '.close',function (){
+        $(this).parents('.uploaded-block').remove();
+    });
+
     // Click Open Nav In Mobile
     $(document).on("click", ".click-nav, .overlay",function() {
         $(".handle").toggleClass("closed");
